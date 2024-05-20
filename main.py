95ff792ec94359cf719accbf7e6858ee95d73bb7
nebdoslab <nebdoslab@gmail.com>
2024-05-20 08:50:51 -0400

    add and fix chunk matching

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ SMALL_NLP_NM = 'en_core_web_sm'
 MEDIUM_NLP_NM = 'en_core_web_md'
 LARGE_NLP_NM = 'en_core_web_lg'
 
+## JSON keys
+PAR_NAME_KEY = 'name'
+PAR_TXT_KEY = 'text'
+OUT_SIM_KEY = 'similarity'
+OUT_MATCH_TXT_KEY = 'matched-txt'
+
+MIN_SIM_TRSHLD = 0.65
+
 # Function to extract text from PDF
 ## Deprecated in PyPDF2 3.0.0: PyPDF2.PdfFileReader, etc.
 # def extract_text_from_pdf(pdf_path):
@@ -50,42 +58,46 @@ def save_to_json(results, output_path='output.json'):
 ##
 def chunk_similarity(paragraphs, pdf_text, nlp):
     results = []
+    pdf_sentences = list(nlp(pdf_text).sents)
     for paragraph in paragraphs:
-        doc1 = nlp(paragraph['text'])
-        paragraph_sentences = list(doc1.sents)
-        num_sentences_in_paragraph = len(paragraph_sentences)
-        pdf_sentences = list(nlp(pdf_text).sents)
-        max_similarity = 0
+        nlp_par = nlp(paragraph[PAR_TXT_KEY])
+        par_sentence_count = len(list(nlp_par.sents))
+        if par_sentence_count < 1:  ## proceed to next paragraph
+            logger.debug(f'No sentences found in paragraph: {paragraph[PAR_NAME_KEY]}')
+            continue
+        max_similarity = 0.0
         matched_text = ''
-        for i in range(len(pdf_sentences) - num_sentences_in_paragraph + 1):
-            chunk = pdf_sentences[i:i+num_sentences_in_paragraph]
+        for i in range(len(pdf_sentences) - par_sentence_count + 1):
+            chunk = pdf_sentences[i:i+par_sentence_count]
             chunk_text = ' '.join([sent.text for sent in chunk])
             doc2 = nlp(chunk_text)
-            similarity = doc1.similarity(doc2)
+            similarity = nlp_par.similarity(doc2)
             if similarity > max_similarity:
                 max_similarity = similarity
                 matched_text = chunk_text
-        if max_similarity > 0.65:
+        if max_similarity > MIN_SIM_TRSHLD:
+            
             results.append({
-                'name': paragraph['name'],
-                'text': paragraph['text'],
-                'matched': matched_text,
-                'similarity': max_similarity
+                PAR_NAME_KEY: paragraph[PAR_NAME_KEY],
+                PAR_TXT_KEY: paragraph[PAR_TXT_KEY],
+                OUT_MATCH_TXT_KEY: matched_text,
+                OUT_SIM_KEY: max_similarity
             })
         else:
             results.append({
-                'name': paragraph['name'],
-                'text': paragraph['text'],
-                'matched': '',
-                'similarity': 0.0
+                PAR_NAME_KEY: paragraph[PAR_NAME_KEY],
+                PAR_TXT_KEY: paragraph[PAR_TXT_KEY],
+                OUT_MATCH_TXT_KEY: '',
+                OUT_SIM_KEY: 0.0
             })
+    return results
 
 ##
 ##
 def simple_similarity(paragraphs, pdf_text, nlp):
     results = []
     for paragraph in paragraphs:
-        doc1 = nlp(paragraph['text'])
+        doc1 = nlp(paragraph[PAR_TXT_KEY])
         max_similarity = 0
         matched_text = ''
         for sent in nlp(pdf_text).sents:
@@ -94,19 +106,19 @@ def simple_similarity(paragraphs, pdf_text, nlp):
             if similarity > max_similarity:
                 max_similarity = similarity
                 matched_text = sent.text
-        if max_similarity > 0.65:
+        if max_similarity > MIN_SIM_TRSHLD:
             results.append({
-                'name': paragraph['name'],
-                'text': paragraph['text'],
-                'matched': matched_text,
-                'similarity': max_similarity
+                PAR_NAME_KEY: paragraph[PAR_NAME_KEY],
+                PAR_TXT_KEY: paragraph[PAR_TXT_KEY],
+                OUT_MATCH_TXT_KEY: matched_text,
+                OUT_SIM_KEY: max_similarity
             })
         else:
             results.append({
-                'name': paragraph['name'],
-                'text': paragraph['text'],
-                'matched': '',
-                'similarity': 0.0
+                PAR_NAME_KEY: paragraph[PAR_NAME_KEY],
+                PAR_TXT_KEY: paragraph[PAR_TXT_KEY],
+                OUT_MATCH_TXT_KEY: '',
+                OUT_SIM_KEY: 0.0
             })
     return results
 
@@ -125,14 +137,15 @@ if __name__ == '__main__':
     pdf_text = extract_text_from_pdf('data/long_text.pdf')
     logger.debug('Loading paragraphs...')
     paragraphs = load_paragraphs('config/paragraphs.json')
-    logger.debug('Calculating SIMPLE similarity...')
-    results = simple_similarity(paragraphs, pdf_text, nlp)
-    logger.debug('\nResults: -----------------')
-    logger.debug(results)
+    # logger.debug('Calculating SIMPLE similarity...')
+    # results = simple_similarity(paragraphs, pdf_text, nlp)
+    # logger.debug('\nResults: -----------------')
+    # logger.debug(results)
     logger.debug('\nCalculating CHUNK similarity...')
     results = chunk_similarity(paragraphs, pdf_text, nlp)
     logger.debug('\nResults: -----------------')
-    logger.debug(results)
+    ## display results in pretty JSON format
+    logger.debug(json.dumps(results, indent=4))
     logger.debug('-------- End! --------')
 
 ####  ----------------------  ####
